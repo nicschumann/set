97843625882f6aea29c321246ccea2d96961dd81
Nic Schumann <nicolas_schumann@brown.edu>
2014-03-14 14:11:44 -0400

    Added deletion to the kdtree.js; generated test files

--- models/js/src/kdtree.js
+++ models/js/src/kdtree.js
@@ -7,19 +7,31 @@ var KDTree = (function() {
 		fail = Error;
 	/** @constructor
 		@param k : int  -- an int representing the dimension of the KDTree
-		@param expect : (a -> b) \/ undefined a function representing the type of object to parameterize over, 
-						or else undefined for a heterogeneous tree
-		@return a kdtree, with kvalue = k */
+		@return a kdtree constructor, with kvalue = k,
+				expecting a collection of KDPoint elements */
 	return function( k ) {
 		return function( kdps ) {
 			var Node = function(value, left, right) {
+
 				this.getValue = function() { return value; }
+				this.setValue = function( val ) { value = val; }
 				this.getLeftChild = function() { return left; }
+				this.setLeftChild = function( val ) { left = val; }
 				this.getRightChild = function() { return right; }
+				this.setRightChild = function( val ) { right = val; }
+
+				this.vector = function() {
+					var vect = [];
+					for ( var i = 0; i < value.arity(); i++ ) {
+						vect[ i ] = value.getK( i );
+					}
+					return vect;
+				}
+
 				this.locates = function( c ) {
 					if ( c.length != value.arity() ) return false;
 					else {
-						for (var i ;= 0; i < value.arity(); i ++ ) {
+						for (var i = 0; i < value.arity(); i ++ ) {
 							if ( value.getK( i ) != c[ i ] ) return false;
 						}
 						return true;
@@ -28,86 +40,131 @@ var KDTree = (function() {
 				this.lookup = function( c, depth ) {
 					if ( this.locates( c ) ) return value;
 					else {
-						if ( (c[i] < value.getK( depth%k )) ) {
+						if ( (c[depth] < value.getK( depth%k )) ) {
 							if ( left.getValue() == undefined ) return null;
-							else return left.get( c, depth+1 );
+							else return left.lookup( c, depth+1 );
 						} else {
 							if ( right.getValue() == undefined ) return null;
-							else return right.get( c, depth+1 );
+							else return right.lookup( c, depth+1 );
 						}
 					}
 				} 
 				this.insert = function( v, depth ) {
-					if ( v.equals( this.value ) ) return false; // duplicate member
+					if ( DEBUG ) console.log( "invoked" );
 
-					if ( v.getK( depth%k ) < this.value.getK( depth%k ) ) {
-						if ( left == null ) {
-							left = v; return true;
+					if ( v.equals( value ) ) return false; // duplicate member
+
+					if ( DEBUG ) console.log( "non-duplicate" );
+
+					if ( v.getK( depth%k ) < value.getK( depth%k ) ) {
+						if ( DEBUG ) console.log( "less" );
+						if ( left.getValue() == undefined ) {
+							if ( DEBUG ) console.log( "left null" );
+							left = new Node( v, new Node(undefined,null,null), new Node(undefined,null,null) ); return true;
 						} else {
 							return left.insert( v, depth+1 );
 						}
 					} else  {
-						if ( right == null ) {
-							right = v; return true;
+						if ( right.getValue() == undefined ) {
+							if ( DEBUG ) console.log( "right null" );
+							right = new Node(v,new Node(undefined,null,null),new Node(undefined,null,null)); return true;
 						} else {
 							return right.insert( v, depth+1 );
 						}
 					}
 				}
-				this.delete = function( c,depth ) {
-					if ( this.locates( c ) ) { // found the point
-						if ( right != null ) {
+				this.print = function( tabbing ) {
+					var str = tabbing;
 
-						} else if ( left!=null ) {
-
-						} else {
-							value = undefined;
-						}
-					} else {
-
+					if ( value != undefined ) {
+						str += value.print()+"\n";
+						str += tabbing+"[L]: " +left.print( tabbing + "\t" )+"\n"; 
+						str += tabbing+"[R]: " +right.print( tabbing + "\t" )+"\n"; 
+						return str;
 					}
+					else return str + "Leaf"
+						
+
 				} 
 			}
 
 			var construct = function( kdps,depth ) {
 				if (kdps.length == 0) return new Node(undefined, null, null);
 
+				if (DEBUG) console.log("passed guard");
+
 				kdps.sort( function( a,b ) {
 					var aval = a.getK( depth % k ),
 						bval = b.getK( depth % k );
-					return aval - bval;
+					return ( aval - bval );
 				});
+
+				if (DEBUG) console.log("passed sorting");
 
 				var m = median( kdps );
 
+				if (DEBUG) console.log("computed median: "+m );
+
 				var l = kdps.filter( function( a ) {
-					return a.getK( depth%k ) < m.getK( depth%k );
-				}),
-				var g = kdps.filter( function( a ) {
-					return a.getK( depth%k ) >= m.getK( depth%k )
-						   && !(a.equals( m ));
+					return a.getK( depth%k ) < kdps[m].getK( depth%k );
 				});
+				if (DEBUG) console.log( l );
+				var g = kdps.filter( function( a ) {
+					return a.getK( depth%k ) >= kdps[m].getK( depth%k )
+						   && !(a.equals( kdps[m] ));
+				});
+				if (DEBUG) console.log( g );
+
+				if (DEBUG) console.log("filtered lists");
 
 				return new Node(
-					m, construct(l, depth+1), construct(r, depth+1)
+					kdps[m], construct(l, depth+1), construct(g, depth+1)
 				);
 			}
 
-			var median = function( kdps ) { return kdps.length / 2; }
+			var remove = function( c, node, depth ) {
+				if ( node.getValue() == undefined ) fail("KDTree::remove(): Deletion of non-existent Node");
+				else if ( node.locates( c ) ) {
+					if ( node.getRightChild() != null ) {
+						node.setValue( minimum( node.getRightChild(), (depth%k), (depth+1)%k ) );
+						node.setRightChild( remove( node.vector(), node.getRightChild(), depth+1 ) );
+					} else if ( node.getLeftChild() != null ) {
+						node.setValue( minimum( node.getLeftChild(), (depth%k), (depth+1)%k ) );
+						node.setRightChild( remove( node.vector(), node.getLeftChild(), depth+1 ) );
+						node.setLeftChild( new Node( undefined, null, null ) );
+					} else {
+						node = new Node( undefined, null, null );
+					}
+				} else if ( c[ depth%k ] < node.getValue().getK( depth%k ) ) {
+					t.setLeftChild( remove( c, node.getLeftChild(), depth+1 ) );
+				} else {
+					t.setRightChild( remove( c, node.getRightChild(), depth+1 ) );
+				}
+				return node;
+			}
 
-			// lookup
+			var minimum = function( node, dim, cd ) {
+				if ( node.getValue() == undefined ) return undefined;
+				if ( cd == dim ) {
+					if ( node.getLeftChild().getValue() == undefined ) return node.getValue();
+					else return minimum(node.getLeftChild(), dim, (cd+1)%k );
+				} else {
+					return least(
+						minimum( node.getLeftChild(), dim, (cd+1)%k ),
+						least( 
+							minimum( node.getRightChild(), dim, (cd+1)%k ),
+						    node.getValue(), dim
+						), dim
+					);
+				}
+			}
 
+			var least = function( val1, val2, dim ) {
+				return ( val1.getK( dim ) < val2.getK( dim ) ) ? val1 : val2;
 
+			}
 
-
-
-
-
-
-
-
-
-
+			var median = function( kdps ) { return Math.floor( kdps.length / 2 ); }
 
 
 			var root = construct( kdps,0 ); 
@@ -118,8 +175,17 @@ var KDTree = (function() {
 					else return root.lookup( c,0 );
 				},
 				insert : function( v ) {
+					if ( DEBUG ) console.log( "invoked" );
+
 					if ( v.arity() != k ) fail("KDTree::insert(): Incorrect K Value");
 					else return root.insert( v,0 );
+				},
+				remove : function( c ) {
+					if ( c.length != k ) fail("KDTree::remove(): Incorrect K Value");
+					else return remove(c, root, 0);
+				},
+				print : function() {
+					return "\n"+root.print("");
 				}
 			};
 		}
@@ -129,25 +195,50 @@ var KDTree = (function() {
 
 
 
-
-var KDPoint = (function() {
-	return function( value, ks ) {
+/**
+ * Given a value to contain, and an array of coordinates, the KDPoint
+ * function returns a point that encapsulates the data, can be located
+ * in K-Dimensional space, and can be compared for equality.
+ */
+var KDPoint = function( value, ks ) {
 		this.constructor = KDPoint;
 
-		this.arity = function() {return ks.length;}
-		this.getk = function( i ) {
+		var local_arity = function() { return ks.length; };
+		var local_getValue = function() { return value; }
+		var local_getK = function( i ) {
 			if ( i < ks.length && i >= 0 ) { return ks[ i ]; }
 			else throw new Error( "No Such Coordinate: "+i+"." );
 		};
-		this.getValue = function() { return value; }
-		this.equals = function( b ) {
-			if ( this.arity() != b.arity() ) return false;
+		var local_equality = function( b ) {
+			if ( local_arity() != b.arity() ) return false;
 			else {
-				for (var i ;= 0; i < this.arity(); i ++ ) {
-					if ( this.getK( i ) != b.getK( i ) ) return false;
+				for (var i = 0; i < local_arity(); i ++ ) {
+					if ( local_getK( i ) != b.getK( i ) ) return false;
 				}
 				return true;
 			}
 		}
-	};
-})();
+		var local_print = function() {
+			var ret = value + ": ( ";
+			for ( var i = 0; i < local_arity(); i++ ) {
+				ret += local_getK( i ) + " "; 
+			}
+			return ret + ")";
+		}
+
+		this.arity = local_arity;
+		this.getK = local_getK;
+		this.getValue = local_getValue;
+		this.equals = local_equality;
+		this.print = local_print;
+};
+
+
+var make2DPoint = function( name, x, y ) { return new KDPoint( name, [x,y] ); }
+var make3DPoint = function( name, x, y, z ) { return new KDPoint( name, [x,y,z] ); }
+
+
+
+
+
+
---
+++ models/js/test/suite_kdtree.js
@@ -0,0 +1,8 @@
+var kdps = [make2DPoint("a",2,3), 
+		    make2DPoint("b",5,4),
+		    make2DPoint("c",9,6),
+		    make2DPoint("d",4,7),
+		    make2DPoint("e",8,1),
+		    make2DPoint("f",7,2) ];
+
+var testTree1 = KDTree(2)(kdps);
