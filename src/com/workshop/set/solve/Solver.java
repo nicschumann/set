a9bb0f3633c447e8ff5e2fe95cdcbcec269e627a
Nic Schumann <nicolas_schumann@brown.edu>
2014-03-29 20:08:01 -0400

    removed Domains

--- src/com/workshop/set/solve/Solver.java
+++ src/com/workshop/set/solve/Solver.java
@@ -2,18 +2,26 @@ package com.workshop.set.solve;
 
 import java.util.Collection;
 
-public class Solver
-             <V extends Variable,
-              D extends Domain,
-              C extends Constraint> {
+import com.workshop.set.interfaces.*;
 
+public class Solver
+             <V extends ISymbol,
+              C extends IConstraint> {
+
+    /**
+     * Each solver is parameterized over a
+     * @param variables
+     * @param constraints
+     */
     public Solver
     (
-            Collection<Variable> variables,
-            Collection<Domain> domains,
-            Collection<Constraint> constraints
+            Collection<ISymbol> variables,
+            Collection<IConstraint> constraints
     ) {
 
     }
+
+
+
 
 }
