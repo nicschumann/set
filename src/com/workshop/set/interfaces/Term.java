40a2249bfb819f2054787ec51e46c64a4eaec6a5
Nic Schumann <nicolas_schumann@brown.edu>
2014-03-29 23:17:17 -0400

    Defined the Term interface

--- src/com/workshop/set/interfaces/Term.java
+++ src/com/workshop/set/interfaces/Term.java
@@ -4,20 +4,30 @@ package com.workshop.set.interfaces;
  * Created by nicschumann on 3/29/14.
  */
 public interface Term {
+
     /**
      * the type that this Term has with respect to the specified IContext.
      *
      * @param gamma the context to check this Term's type against
-     * @return this ITerm's type in g
+     * @return this term's type in gamma
      */
-    public Term typesAs( Context gamma );
+    public Term type( Context gamma );
 
     /**
-     * the value that this Term evaluates to with respect to a specified context g
+     * The term that this term produces under one step of evaluation
      *
      * @param eta the environment to evaluate this term in
-     * @return this term's value in g
+     * @return this term's next step in eta.
      */
-    public Value evaluatesTo( Environment eta );
+    public Term step( Environment eta );
+
+    /**
+     *
+     * The value that this term reduces to under full reduction.
+     *
+     * @param eta an environment to evaluate this term in.
+     * @return this term's value in eta
+     */
+    public Value evaluate( Environment eta );
 
 }
