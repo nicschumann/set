fa2b3d735c0bb4901a54d2f8cc69a986f5a746ca
Nic Schumann <nicolas_schumann@brown.edu>
2014-04-20 13:43:37 -0400

    Added Constraint Classes

---
+++ src/com/workshop/set/model/ConstraintMatrix.java
@@ -0,0 +1,28 @@
+package com.workshop.set.model;
+
+import com.google.common.collect.Table;
+
+import com.workshop.set.model.interfaces.Constraint;
+import com.workshop.set.model.interfaces.Symbol;
+
+/**
+ * Created by nicschumann on 4/17/14.
+ */
+public class ConstraintMatrix {
+
+    /**
+     * given a set of n > 0 constraint maps, constraint matrix constructs a table of constraints,
+     * 
+     * @param maps
+     */
+    public ConstraintMatrix( ConstraintMap... maps ) {
+
+
+    }
+
+    private Table<Constraint,Symbol,Mutable<Double>> matrix;
+
+
+
+
+}
