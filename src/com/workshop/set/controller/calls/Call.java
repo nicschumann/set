5de6d764975e7ea8529fe149251bfe7dc486c67f
Nic Schumann <nicolas_schumann@brown.edu>
2014-04-20 14:39:29 -0400

    Call primitive

---
+++ src/com/workshop/set/controller/calls/Call.java
@@ -0,0 +1,30 @@
+package com.workshop.set.controller.calls;
+
+import com.workshop.set.model.Model;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Created by nicschumann on 4/16/14.
+ */
+public abstract class Call<T> implements Callable<T> {
+
+//    protected Model m;
+//
+//
+//
+//    public void bind( Model m ) {
+//
+//    }
+//
+//    public T call() {
+//        if ( m == null ) {
+//
+//        }
+//    }
+//
+//
+//
+//    public abstract T handle();
+
+}
