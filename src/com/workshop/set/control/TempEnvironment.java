a6dd029a3cffb1084a5d94bef0fed878009f4931
barneslt63@gmail.com <logan_barnes@brown.edu>
2014-05-05 13:55:43 -0400

    raytracing works for lines

--- src/com/workshop/set/control/TempEnvironment.java
+++ src/com/workshop/set/control/TempEnvironment.java
@@ -33,6 +33,8 @@ import com.workshop.set.model.interfaces.Model;
 import com.workshop.set.view.SetScreen;
 
 public class TempEnvironment implements Model {
+	
+	public static final float SELECTION_RADIUS = 0.1f;
 
 	private SetScreen _screen;
 
@@ -439,6 +441,8 @@ public class TempEnvironment implements Model {
 	@Override
 	public Geometry getGeometry(Vector3f A, Vector3f B, Point p) {
 
+		Geometry selected = null;
+		
 		for (Geometry element : _currentElements) {
 			if (element instanceof Point) {
 
@@ -456,52 +460,96 @@ public class TempEnvironment implements Model {
 				float b = 2 * ((bMinA.x * aMinC.x) + (bMinA.y * aMinC.y) + (bMinA.z * aMinC.z));
 
 				// c
-				float c = (aMinC.x * aMinC.x) + (aMinC.y * aMinC.y) + (aMinC.z * aMinC.z) - 0.08f * 0.08f;
+				float c = (aMinC.x * aMinC.x) + (aMinC.y * aMinC.y) + (aMinC.z * aMinC.z) - SELECTION_RADIUS * SELECTION_RADIUS;
 
 				if ((b * b - 4 * a * c) >= 0)
-					return element;
+					selected = element;
 			} else {
 				double[] pnts = element.getPointArray();
 				if (pnts.length == 6) { // line
 
-					if (this.checkLineIntersection(p, pnts))
-						return element;
-//					Vector3f L = new Vector3f((float) pnts[0], (float) pnts[1], (float) pnts[2]);
+//					if (this.checkLineIntersection(p, pnts))
+//						return element;
+					Vector3f L = new Vector3f((float) pnts[0], (float) pnts[1], (float) pnts[2]);
+					Vector3f L2 = new Vector3f((float) pnts[3], (float) pnts[4], (float) pnts[5]);
 //					
-//					Vector3f dir1 = new Vector3f();
-//					Vector3f dir2 = new Vector3f();
-//					Vector3f.sub(new Vector3f((float) pnts[3], (float) pnts[4], (float) pnts[5]), L, dir1);
-//					Vector3f.sub(B, A, dir2);
-//					dir1.normalise();
-//					dir2.normalise();
+					Vector3f dir1 = new Vector3f();
+					Vector3f dir2 = new Vector3f();
+					Vector3f.sub(L2, L, dir1);
+					Vector3f.sub(B, A, dir2);
+					dir1.normalise();
+					dir2.normalise();
 					
 //					Vector3f L = new Vector3f(0, 2, -1);
 //					A = new Vector3f(1, 0, -1);
 //					Vector3f dir1 = new Vector3f(1, 1, 2);
 //					Vector3f dir2 = new Vector3f(1, 1, 3);
-//					
-//					Vector3f aMinL = new Vector3f();
-//					Vector3f.sub(A, L, aMinL);
-//					
-//					float c1 = Vector3f.dot(aMinL, dir1);
-//					float c2 = Vector3f.dot(dir1, dir2);
-//					float c3 = Vector3f.dot(dir1, dir1);
-//					float c4 = Vector3f.dot(aMinL, dir2);
-//					float c5 = Vector3f.dot(dir2, dir2);
-//
-//					float t = (c2*c4 - c1) / (c5*c2 - c3);
-//					float s = (t*c3 - c1) / c2;
-//					
-//					System.out.println(s + ", " + t);
-//					
-//					Vector3f P = new Vector3f(L.x + t*dir1.x, L.y + t*dir1.y, L.z + t*dir1.z);
-//					Vector3f Q = new Vector3f(A.x + s*dir2.x, A.y + s*dir2.y, A.z + s*dir2.z);
 					
+					Vector3f aMinL = new Vector3f();
+					Vector3f.sub(A, L, aMinL);
+					
+					float c1 = Vector3f.dot(aMinL, dir1);
+					float c2 = Vector3f.dot(dir1, dir2);
+					float c3 = Vector3f.dot(dir1, dir1);
+					float c4 = Vector3f.dot(aMinL, dir2);
+					float c5 = Vector3f.dot(dir2, dir2);
+					float c6 = c2;
+
+					float s = (c1*c6 - c4*c3) / (c5*c3 - c6*c2);
+					float t = (s*c2 + c1) / c3;
+					
+//					System.out.println(c4 + ", " + c5 + ", " + c6);
+//					System.out.println(s + ", " + t);
+					
+					Vector3f P = new Vector3f(L.x + t*dir1.x, L.y + t*dir1.y, L.z + t*dir1.z);
+					Vector3f Q = new Vector3f(A.x + s*dir2.x, A.y + s*dir2.y, A.z + s*dir2.z);
+					if (!betweenPoint(L, L2, P)) {
+						continue;
+					}
+					
+					Vector3f PQ = new Vector3f();
+					Vector3f.sub(Q, P, PQ);
+					
+					double d = Math.sqrt(Vector3f.dot(PQ, PQ));
+					if (d < 0.05f && selected == null)
+						 selected = element;
 					
 				}
 			}
 		}
-		return null;
+		return selected;
+	}
+	
+	public static boolean betweenPoint(Vector3f p1, Vector3f p2, Vector3f p) {
+		if (p1.x < p2.x) {
+			if (p.x < p1.x || p.x > p2.x) {
+				return false;
+			}
+		} else {
+			if (p.x < p2.x || p.x > p1.x) {
+				return false;
+			}
+		}
+		if (p1.y < p2.y) {
+			if (p.y < p1.y || p.y > p2.y) {
+				return false;
+			}
+		} else {
+			if (p.y < p2.y || p.y > p1.y) {
+				return false;
+			}
+		}
+		if (p1.z < p2.z) {
+			if (p.z < p1.z || p.z > p2.z) {
+				return false;
+			}
+		} else {
+			if (p.z < p2.z || p.z > p1.z) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 
 }
